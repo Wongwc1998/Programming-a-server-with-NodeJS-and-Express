586357fc538d95c4362ac49fa884300e24089c8e
Wongwc1998 <wongwc1998@gmail.com>
2023-09-25 16:44:06 -0400

    add validator

--- models/person.js
+++ models/person.js
@@ -17,8 +17,20 @@ mongoose
   });
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minlength: 3,
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    validate: {
+      validator: function (v) {
+        //be formed of two parts that are separated by -, the first part has two or three numbers and the second part also consists of numbers
+        return /^\d{2,3}-\d+$/.test(v);
+      },
+    },
+  },
 });
 
 personSchema.set("toJSON", {
